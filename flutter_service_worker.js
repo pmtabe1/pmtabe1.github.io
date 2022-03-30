27517541019761b7d06cf52438f5008468ed92d5
Paul Msegeya <92868240+pmtabe1@users.noreply.github.com>
2022-03-30 16:40:33 +0300

    another deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e6707d767e32f4f92309813500806013",
-"index.html": "d5bf98fc8f4b5b1838b97166b925f38c",
-"/": "d5bf98fc8f4b5b1838b97166b925f38c",
-"main.dart.js": "ec66aaf0eee72d44d82666cffc976903",
+"index.html": "138714bf9d127d1243142ab4fac1eb6e",
+"/": "138714bf9d127d1243142ab4fac1eb6e",
+"main.dart.js": "ff18dae09b07d8ff5e1c980425cad84a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
