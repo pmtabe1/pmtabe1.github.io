1be318f59d3e581fb166c37eee45a44601a21da9
Paul Msegeya <92868240+pmtabe1@users.noreply.github.com>
2022-05-09 18:19:00 +0300

    another deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e6707d767e32f4f92309813500806013",
-"index.html": "c5c79a67e8b7e2bc51ef735ddc934902",
-"/": "c5c79a67e8b7e2bc51ef735ddc934902",
-"main.dart.js": "45ad5c2b243a6e905c71ecd489c2aa50",
+"index.html": "73b08fba26252bbf4c3ff5a89b564c21",
+"/": "73b08fba26252bbf4c3ff5a89b564c21",
+"main.dart.js": "2b30677e6e2dda7194711944598d7185",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "91dde8b0c76b213b7b9af3a3ecdc674d",
 "assets/AssetManifest.json": "09de320142026a83edd4e273d587b0ba",
-"assets/NOTICES": "1622f1b33dc257f09f416ae11840627b",
+"assets/NOTICES": "d08a56f5dd5bdcf45d8236fac930b965",
 "assets/FontManifest.json": "5aa53278e253b596398920140c22353a",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Script-Regular.ttf": "55d2dcd4778875a53ff09320a85a5296",
